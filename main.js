44894dd7e7a2845f9eeb5599f992ba6bbba077e7
Barry Rhodes <barry@mymedialab.co.uk>
2017-02-09 17:40:44 +0000

    Little bit o writing to the dom

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
-var $form = $('#ticket-form');
+var tt = {};
 
-$form.on('submit', function(e) {
-    var values = {}, $inputs;
+tt.form = function($form) {
+    $form.on('submit', function(e) {
+        var values = {}, $inputs;
 
-    e.preventDefault();
-    $inputs = $form.find('select, textarea, input');
-    $inputs.each(function(key, el) {
-        var name = el.name;
-        var value = el.value;
-        if (name) {
-            values[name] = value;
-        }
+        e.preventDefault();
+        $inputs = $form.find('select, textarea, input');
+        $inputs.each(function(key, el) {
+            var name = el.name;
+            var value = el.value;
+            if (name) {
+                values[name] = value;
+            }
+        });
+
+        window.localStorage.setItem('ticket', JSON.stringify(values));
     });
+};
 
-    window.localStorage.setItem('ticket', JSON.stringify(values));
-});
+tt.list = function($list) {
+    var template = '<li class="ticket-list__item"><h4 class="ticket-list__title">{{ title }}</h4><span class="ticket-list__user">{{ name }}</span></li>';
+    var values = JSON.parse(window.localStorage.getItem('ticket'));
+
+    template = template.replace('{{ title }}', values.details);
+    template = template.replace('{{ name }}', values.name);
+
+    $list.html(template);
+};
+
+tt.main = function() {
+    var $form = $('#ticket-form');
+    var $list = $('#ticket-list');
+
+    tt.form($form);
+    tt.list($list);
+};
+
+tt.main();
